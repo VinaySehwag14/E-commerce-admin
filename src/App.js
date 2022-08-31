ebf5e8c04df89ffc8dfa1702a4939a07c7f19d8e
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-08-31 15:03:22 +0530

    added dynamic token access and conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
@@ -20,42 +15,30 @@ import { useSelector } from "react-redux";
 function App() {
   const admin = useSelector((state) => state.user.currentUser.isAdmin);
   return (
-    <Router>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        {admin && (
-          <>
-            <Topbar />
-            <div className="container">
-              <Sidebar />
-              <Route exact path="/">
-                <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      {admin && (
+        <>
+          <Topbar />
+          <div className="container">
+            <Sidebar />
+            <Routes>
+              <Route exact path="/" element={<Home />}>
+                <Route path="/users" element={<UserList />} />
+                <Route path="/user/:userId" element={<User />} />
+                <Route path="/newUser" element={<NewUser />} />
+                <Route path="/products" element={<ProductList />} />
+                <Route path="/product/:productId" element={<Product />} />
+                <Route path="/newproduct" element={<NewProduct />} />
               </Route>
-              <Route path="/users">
-                <UserList />
-              </Route>
-              <Route path="/user/:userId">
-                <User />
-              </Route>
-              <Route path="/newUser">
-                <NewUser />
-              </Route>
-              <Route path="/products">
-                <ProductList />
-              </Route>
-              <Route path="/product/:productId">
-                <Product />
-              </Route>
-              <Route path="/newproduct">
-                <NewProduct />
-              </Route>
-            </div>
-          </>
-        )}
-      </Switch>
-    </Router>
+            </Routes>
+          </div>
+        </>
+      )}
+    </BrowserRouter>
   );
 }
 
